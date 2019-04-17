928f5e058265f6481c08844a5b184cb556a64304
Allison Snipes <ajsnipes@usfca.edu>
2019-04-16 21:29:40 -0700

    demo page

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,8 @@ document.querySelector("#container").addEventListener("click", function(event) {
   if(event.target.tagName == "BUTTON")
 }
 
-// the geolocating API from MDN geolocat --getcurrentposition method with the parameter of position
+// the geolocating MDN API
+//getcurrentposition method with the parameter of position
 navigator.geolocation.getCurrentPosition(function(position) {
   //the do something function will execute when the location is obtained
   //defining instrustions based on the property of the coordinates of long and lat
@@ -23,30 +24,6 @@ navigator.geolocation.clearWatch(watchID);
 function errorCallback(error) {
   alert('ERROR(' + error.code + '): ' + error.message);
 };
-// grab the latitude and longitude from the user
-//add event listner to the button to be clicked 
-document.querySelector("#clickMe").addEventListener("click",function(event){
-  event.preventDefault();
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-var latitude,longitude;
 
-function success(pos) {
-  var crd = pos.coords;
-  latitude = crd.latitude;
-  longitude = crd.longitude;
+//using open street map to get location from latitude and longitude
 
-  console.log('Your current position is:');
-  console.log(`Latitude : ${latitude}`);
-  console.log(`Longitude: ${longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);})
